c8d38754360ae14aabeb6845c32df9602442a846
Matthew Saw <matthew.saw@decathlon.com>
2019-12-16 13:53:01 +0100

    Init continued

--- src/Student.java
+++ src/Student.java
@@ -6,8 +6,8 @@ public class Student {
   private String firstName;
   private String lastName;
   private int year;
-  private String courses;
-  private double tuitionBalance;
+  private String courses = "";
+  private int tuitionBalance = 0;
   private static int costOfCourse = 600;
   private static int id = 1000;
 
@@ -23,14 +23,48 @@ public class Student {
     this.year = in.nextInt();
 
     setStudentId();
-
-    System.out.println(firstName + " " + lastName + " " + "\nStudent ID: " + studentId + "\nYear: " + year);
-
-
   }
 
   private void setStudentId() {
     id++;
-   this.studentId = year + "" + id;
+    this.studentId = year + "" + id;
+  }
+
+  public void enroll() {
+    do {
+      System.out.println("Enter course to enroll (Q to quit): ");
+      Scanner in = new Scanner(System.in);
+      String course = in.nextLine();
+      if (!course.equals("Q")) {
+        courses = courses + "\n - " + course;
+        tuitionBalance = tuitionBalance + costOfCourse;
+      } else {
+        break;
+      }
+    }
+    while (1 != 0);
+    //System.out.println("You are enrolled in: " + courses);
+  }
+
+  public void viewBalance(){
+    System.out.println("Your tuition balance is: $" + tuitionBalance);
+  }
+
+  public void payTuition(){
+    viewBalance();
+    System.out.print("Please enter the amount you would like to pay: ");
+    Scanner in = new Scanner(System.in);
+    int payment = in.nextInt();
+    tuitionBalance = tuitionBalance - payment;
+    System.out.println("Thank you for your payment of $" + payment);
+    viewBalance();
+  }
+
+  public void showInfo (){
+    System.out.println("\nName: "+ firstName + " " + lastName);
+    System.out.println("Student id: "+ studentId);
+    System.out.println("Year: " + year);
+    System.out.println("Courses enrolled: " + courses);
+    System.out.println("Tuition balance: $" + tuitionBalance);
   }
 }
--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -2,5 +2,8 @@ public class StudentDatabaseApp {
 
   public static void main(String[] args) {
     Student stu1 = new Student();
+    stu1.enroll();
+    stu1.payTuition();
+    stu1.showInfo();
   }
 }
